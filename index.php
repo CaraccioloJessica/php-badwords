d4dc28b679dc9b31da07616abbdf2c3bb9a348c0
Jessica <jesscara.993@gmail.com>
2023-01-09 16:14:31 +0100

    created $censored, GET parameter, divided in 2 div

--- index.php
+++ index.php
@@ -7,18 +7,58 @@
   <title>Badwords</title>
 </head>
 <body>
-  <h1>
-    <?php
-    $text = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor maiores sunt molestiae omnis numquam et. Praesentium suscipit impedit inventore quis dolores magni unde dolore, ad mollitia a fuga facilis voluptates.';
-    echo $text;
-    ?>
-    </h1>  
+  <!-- Raccolta variabili php -->
+  <?php
+  // parola verrà cambiata tramite url
+  $word = $_GET['badWord'];
+  $text = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor maiores sunt molestiae omnis numquam et. Praesentium suscipit impedit inventore quis dolores magni unde dolore, ad mollitia a fuga facilis voluptates.';
+  $censored = str_replace("$word", "***", "$text");
+  ?>
+  <!-- fine raccolta variabili -->
 
-    <h2>
-      Lunghezza paragrafo:
-      <?php
-      echo strlen($text);
-      ?>
-    </h2>
+  <!-- Titolo esercizio con spiegazione -->
+  <header>
+    <h1> PHP Badwords </h1>
+  </header>
+
+  <main>
+    <!-- stampa paragrafo non cencurato -->
+    <div>
+      <!-- titolo  -->
+      <h2>
+        Paragrafo:
+      </h2>
+      
+      <!-- testo intatto -->
+      <h3>
+        <?php echo $text ?>
+      </h3>
+    
+      <!-- conteggio lunghezza testo -->
+      <h4>
+        Lunghezza paragrafo:
+        <?php echo strlen($text); ?>
+      </h4>
+    </div>
+  
+    <!-- stampa paragrafo con parola censurata scelta da utente -->
+    <div>
+      <!-- titolo -->
+      <h2>
+        Paragrafo con censura:
+      </h2>
+
+      <!-- testo con censura -->
+      <h3>
+        <?php echo $censored ?>
+      </h3>
+    
+      <!-- conteggio lunghezza testo con censura-->
+      <h4>
+        Lunghezza paragrafo censurato:
+        <?php echo strlen($censored); ?>
+      </h4>
+    </div>
+  </main>
 </body>
 </html>
